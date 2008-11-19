c8e10251d8102c77ba9b44e1217fb10f1ad2ae85
Vytautas Šaltenis <Vytautas.Shaltenis@gmail.com>
2008-11-20 00:28:24 +0200

    Moved test files to separate directory
    ...for easier tagging.

---
+++ c-files/td.cpp
@@ -0,0 +1,29 @@
+
+/**
+ * \brief This function does magick. Another mystype. Third sentence.
+ * Fourth. Fifth. Sntnc.
+ * Bz bz
+ *
+ * \param pPtr The pointer. Foo. \ref gazonk.
+ * baz
+ * xyzzy
+ *
+ * \retval NULL         Error has occurred. Foo.
+ * \retval Otherwise    Magic pointer. Foo.
+ */
+char* magick (char *pPtr);
+
+void gazonk ();
+
+void gazonk ()
+{
+    return
+}
+
+/**
+ * Group of definitions. Mystype.
+ */
+#define DEF_FOO 1   /**< Definition of foo. */
+#define DEF_BAR 2   /**< Definition of bar. */
+#define DEF_GOO 3   /**< Definition of goo. */
+
