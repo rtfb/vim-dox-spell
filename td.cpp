2b35711970d2e3a2065b5df2749f2310d542f952
Vytautas Šaltenis <Vytautas.Shaltenis@gmail.com>
2008-11-19 21:39:24 +0200

    Initial (original) doxygen.vim and a test file
    Adding the original doxygen.vim script to track my hacks. A simple test
    file is also here.

---
+++ td.cpp
@@ -0,0 +1,16 @@
+
+/**
+ * \brief This function does magick.
+ * Bz bz
+ *
+ * \param pPtr The pointer. Foo. \ref gazonk.
+ * baz
+ * xyzzy
+ *
+ * \retval NULL         Error has occurred. Foo.
+ * \retval Otherwise    Magic pointer. Foo.
+ */
+char* magick (char *pPtr);
+
+void gazonk ();
+
